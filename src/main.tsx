3c3fd0b5cef809b787f169b2e6a20112cb576fe3
Andrei <zoitber9@mail.ru>
2023-08-16 01:22:35 +0300

    update component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './styles/index.scss'
+
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
-import { App } from './App.tsx'
+import { App } from './App'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
