0347bfcd611fa071f277d2389dde93bc25ec278e
Andrei <zoitber9@mail.ru>
2023-08-17 17:44:51 +0300

    refactor typography component

--- src/components/ui/typography/typography.stories.tsx
+++ src/components/ui/typography/typography.stories.tsx
@@ -2,23 +2,38 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Typography } from './'
 
-const meta: Meta = {
+const variantOptions = [
+  'large',
+  'h1',
+  'h2',
+  'h3',
+  'body1',
+  'body2',
+  'subtitle1',
+  'subtitle2',
+  'caption',
+  'overline',
+  'link1',
+  'link2',
+]
+
+const colorOptions = ['primary', 'secondary', 'inherit', 'link', 'error']
+
+const meta = {
   title: 'Components/Typography',
   component: Typography,
   tags: ['autodocs'],
   argTypes: {
-    options: [
-      'large',
-      'h1',
-      'h2',
-      'h3',
-      'body1',
-      'body2',
-      'subtitle1',
-      'subtitle2',
-      'link1',
-      'link2',
-    ],
+    variant: {
+      options: variantOptions,
+      control: { type: 'radio' },
+    },
+    color: {
+      options: colorOptions,
+      control: { type: 'radio' },
+    },
+    className: { control: 'text' },
+    children: { control: 'text' },
   },
 } satisfies Meta<typeof Typography>
 
@@ -28,83 +43,123 @@ type Story = StoryObj<typeof meta>
 export const Large: Story = {
   args: {
     variant: 'large',
-    children: 'large Typography',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const H1: Story = {
   args: {
-    variant: 'H1',
-    children: 'H1 Typography',
+    variant: 'h1',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const H2: Story = {
   args: {
-    variant: 'H2',
-    children: 'H2 Typography',
+    variant: 'h2',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const H3: Story = {
   args: {
-    variant: 'H3',
-    children: 'H3 Typography',
+    variant: 'h3',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const Body1: Story = {
   args: {
     variant: 'body1',
-    children: 'Body1 Typography',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const Body2: Story = {
   args: {
     variant: 'body2',
-    children: 'Body2 Typography',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const Subtitle1: Story = {
   args: {
     variant: 'subtitle1',
-    children: 'Subtitle1 Typography',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const Subtitle2: Story = {
   args: {
     variant: 'subtitle2',
-    children: 'Subtitle2 Typography',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
-export const Caption: Story = {
+export const CaptionWithError: Story = {
   args: {
     variant: 'caption',
-    children: 'Caption Typography',
+    color: 'error',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const Overline: Story = {
   args: {
     variant: 'overline',
-    children: 'Overline Typography',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const Link1: Story = {
   args: {
     variant: 'link1',
-    children: 'Link1 Typography',
+    color: 'link',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
-
 export const Link2: Story = {
   args: {
     variant: 'link2',
-    children: 'Link2 Typography',
+    color: 'link',
+    children: (
+      <>
+        Carosserie Test Zürich <br /> Stauffacherstrasse 31 <br /> 8004 Zürich, ZH, CH
+      </>
+    ),
   },
 }
--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,29 +1,34 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
 import s from './typography.module.scss'
 
+type VariantType =
+  | 'large'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'body1'
+  | 'body2'
+  | 'subtitle1'
+  | 'subtitle2'
+  | 'caption'
+  | 'overline'
+  | 'link1'
+  | 'link2'
+
+type ColorType = 'primary' | 'secondary' | 'inherit' | 'error' | 'link'
+
 export type TypographyProps<T extends ElementType = 'p'> = {
   as?: T
+  variant: VariantType
+  children?: ReactNode
   className?: string
-  variant?:
-    | 'large'
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'body1'
-    | 'body2'
-    | 'subtitle1'
-    | 'subtitle2'
-    | 'overline'
-    | 'caption'
-    | 'link1'
-    | 'link2'
+  color?: ColorType
 } & ComponentPropsWithoutRef<T>
 
-export const Typography = <T extends ElementType = 'p'>(
-  props: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>
-) => {
-  const { variant = 'large', className, as: Component = 'p', ...rest } = props
+export const Typography = <T extends ElementType = 'p'>(props: TypographyProps<T>) => {
+  const { variant = 'body1', className, as: Component = 'p', color = 'primary', ...rest } = props
+  const clN = `${s[variant]} ${className ?? ''} ${s[color]}`
 
-  return <Component className={s[variant]} {...rest} />
+  return <Component className={clN} {...rest} />
 }
