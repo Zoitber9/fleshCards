6e51d97fa8b9fe34792a58694485d553ee21826c
Andrei <zoitber9@mail.ru>
2023-08-18 23:52:40 +0300

    add radio-button component

---
+++ src/components/ui/radio-button/radio-button.tsx
@@ -0,0 +1,32 @@
+import { CSSProperties, ComponentPropsWithoutRef } from 'react'
+
+import clsx from 'clsx'
+
+import { Typography } from '../typography'
+
+import s from './radio-button.module.scss'
+
+type Props = {
+  label?: string
+  className?: string
+  style?: CSSProperties
+  disabled?: boolean
+}
+
+export const RadioButton = (
+  props: Props & Omit<ComponentPropsWithoutRef<'input'>, keyof Props | 'type'>
+) => {
+  const { label, disabled, className, style, ...restProps } = props
+
+  return (
+    <label
+      className={clsx(s.label, disabled ? s.disabled : undefined, className) || undefined}
+      style={style}
+    >
+      <input type="radio" className={s.radio} disabled={disabled} {...restProps} />
+      <Typography as="span" variant="body2">
+        {label}
+      </Typography>
+    </label>
+  )
+}
