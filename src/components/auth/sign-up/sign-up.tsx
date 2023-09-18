b3c083d5480bb3276f5c549e5ab23a69b0964366
Andrei <zoitber9@mail.ru>
2023-09-19 00:07:01 +0300

    add sign-un components and stories

--- src/components/auth/sign-up/sign-up.tsx
+++ src/components/auth/sign-up/sign-up.tsx
@@ -3,26 +3,37 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-import s from './sign-in.module.scss'
+import s from './sign-up.module.scss'
 
 import { Button, Card, Typography } from '@/components/ui'
-import { ControlledCheckbox } from '@/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx'
 import { ControlledTextField } from '@/components/ui/controlled/controlled-text-field/controlled-text-field.tsx'
 
-const loginSchema = z.object({
-  email: z
-    .string()
-    .trim()
-    .email('Invalid email address')
-    .nonempty('Enter email')
-    .min(3, 'Login must be at least 3 characters'),
-  password: z
-    .string()
-    .trim()
-    .nonempty('Enter password')
-    .min(8, 'Password must be at least 8 characters'),
-  rememberMe: z.boolean().optional(),
-})
+const loginSchema = z
+  .object({
+    email: z
+      .string()
+      .trim()
+      .email('Invalid email address')
+      .nonempty('Enter email')
+      .min(3, 'Login must be at least 3 characters'),
+    password: z
+      .string()
+      .trim()
+      .nonempty('Enter password')
+      .min(8, 'Password must be at least 8 characters'),
+    passwordConfirmation: z.string().trim().nonempty('Enter password'),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password !== data.passwordConfirmation) {
+      ctx.addIssue({
+        message: 'Passwords do not match',
+        code: z.ZodIssueCode.custom,
+        path: ['passwordConfirmation'],
+      })
+    }
+
+    return data
+  })
 
 type FormType = z.infer<typeof loginSchema>
 
@@ -30,14 +41,14 @@ type Props = {
   onSubmit: (data: FormType) => void
 }
 
-export const SignIn = (props: Props) => {
+export const SignUp = (props: Props) => {
   const { control, handleSubmit } = useForm<FormType>({
     mode: 'onSubmit',
     resolver: zodResolver(loginSchema),
     defaultValues: {
       email: '',
       password: '',
-      rememberMe: false,
+      passwordConfirmation: '',
     },
   })
 
@@ -62,18 +73,22 @@ export const SignIn = (props: Props) => {
             control={control}
             label={'Password'}
           />
-          <ControlledCheckbox name={'rememberMe'} control={control} label={'Remember me'} />
-          <Typography className={s.linkRecoverPassword} variant={'body2'} as={'a'} href={'/'}>
-            Forgot Password?
-          </Typography>
+          <ControlledTextField
+            placeholder={'Confirm password'}
+            type={'password'}
+            name={'passwordConfirmation'}
+            control={control}
+            label={'Confirm password'}
+          />
+
           <Button fullWidth={true} type="submit">
             Sign In
           </Button>
           <Typography className={s.caption} variant="body2">
-            Don`t have an account?
+            Already have an account?
           </Typography>
-          <Typography variant="link1" as={'a'} href="/sign-up" className={s.linkSignUp}>
-            Sign Up
+          <Typography variant="link1" as={'a'} href="/" className={s.linkSignIn}>
+            Sign In
           </Typography>
         </form>
       </Card>
