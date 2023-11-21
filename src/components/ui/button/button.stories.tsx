316f6c325a5239b630d3806096eceb00744fc08c
Andrei <zoitber9@mail.ru>
2023-11-22 01:42:51 +0300

    /

---
+++ src/components/ui/button/button.stories.tsx
@@ -0,0 +1,84 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { LogoutSvg } from '@/assets/icons/logout'
+import { Button } from '@/components/ui/button/button'
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary', 'link'],
+      control: { type: 'radio' },
+    },
+    fullWidth: {
+      options: [true, false],
+      control: { type: 'radio' },
+    },
+    as: { control: 'text' },
+    children: { control: 'text' },
+  },
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Primary button',
+    disabled: false,
+  },
+}
+export const PrimaryWithIcon: Story = {
+  args: {
+    variant: 'primary',
+    children: (
+      <>
+        <LogoutSvg />
+        Primary button
+      </>
+    ),
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Button',
+    disabled: false,
+  },
+}
+export const Tertiary: Story = {
+  args: {
+    variant: 'tertiary',
+    children: 'Tertiary Button',
+    disabled: false,
+  },
+}
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Link Button',
+    disabled: false,
+  },
+}
+
+export const AsLink: Story = {
+  args: {
+    variant: 'tertiary',
+    children: 'Link that looks like a button',
+    as: 'a',
+    href: 'https://google.com',
+  },
+}
+
+export const FullWidth: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Full Width Button',
+    disabled: false,
+    fullWidth: true,
+  },
+}
