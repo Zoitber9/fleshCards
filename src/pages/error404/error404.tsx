9eb46954ae2a91fae5a09d0e92ded95adf56ff65
Andrei <zoitber9@mail.ru>
2023-11-27 17:49:35 +0300

    add error404 page

---
+++ src/pages/error404/error404.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+import s from 'src/components/error404/error404.module.scss'
+
+import error404 from '@/assets/images/error404.png'
+import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
+
+const Error404 = () => {
+  const navigate = useNavigate()
+  const backHomeHandler = () => {
+    navigate('/')
+  }
+
+  return (
+    <div className={s.container}>
+      <img className={s.image} src={error404} alt="ERROR 404" />
+      <Typography className={s.text} variant={'h3'}>
+        Sorry! Page not found!
+      </Typography>
+      <Button className={s.btn} onClick={backHomeHandler}>
+        Back to home page
+      </Button>
+    </div>
+  )
+}
+
+export default Error404
