f0336acfa989f7b7c6d6f26c0b34b457e1a69491
Andrei <zoitber9@mail.ru>
2023-12-08 23:57:26 +0300

    add custom appDispatch

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 import { decksApi } from '@/services/decks-api/decks-api'
 
@@ -11,3 +12,7 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
